e11e65a011649e0b9dbb58c573e47e5445a10b28
Felipe Eduardo <felipe.eduardo@embedded.ufcg.edu.br>
2014-07-23 16:48:53 -0300

    adcionado codigos iniciais para o projeto.
    Pegos do projeto piloto de les.

---
+++ thiner-app/src/com/thiner/asynctask/PostJSONTask.java
@@ -0,0 +1,69 @@
+
+package com.thiner.asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.thiner.utils.MyLog;
+import com.thiner.utils.PotmUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class PostJSONTask extends AsyncTask<String, Void, String> {
+    private final Context mContext;
+
+    public PostJSONTask(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected String doInBackground(String... paramns) {
+
+        int trys = 10;
+        String result = null;
+
+        while (trys > 0) {
+            trys--;
+
+            try {
+
+                MyLog.info("POST: " + PotmUtils.getServerURL() + " - " + paramns[0]);
+
+                result = PotmUtils.sendPOST(PotmUtils.getServerURL(), paramns[0]);
+
+                if (result != null) {
+                    break;
+                }
+
+            } catch (final IOException e) {
+                MyLog.debug("Unable to retrieve web page. URL may be invalid: "
+                        + PotmUtils.getServerURL() + " - " + paramns[0]);
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (final InterruptedException e) {
+                MyLog.error(e.getMessage());
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+        try {
+            ((PostJSONInterface) mContext).callbackPostJSON(new JSONObject(msg));
+        } catch (JSONException e) {
+            MyLog.error("Error when creating JSON on PostJSONTask", e);
+        }
+
+    }
+
+    public interface PostJSONInterface {
+        public void callbackPostJSON(JSONObject msg);
+    }
+}
---
+++ thiner-app/src/com/thiner/utils/MyLog.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2014 Embedded Systems and Pervasive Computing Lab - UFCG
+ * All rights reserved.
+ */
+package com.thiner.utils;
+
+import android.util.Log;
+
+/**
+ * The Class MyLog.
+ */
+public final class MyLog {
+
+	private MyLog(){
+		
+	}
+    /**
+     * The Constant TAG.
+     */
+    private static final String TAG = "POMT";
+
+    /**
+     * The Constant ERROR.
+     */
+    private static final String ERROR = "ERROR";
+
+    /**
+     * The Constant WARNING.
+     */
+    private static final String WARNING = "WARNING";
+
+    /**
+     * The Constant INFO.
+     */
+    private static final String INFO = "INFO";
+
+    /**
+     * The Constant DEBUG.
+     */
+    private static final String DEBUG = "DEBUG";
+
+    /**
+     * The Constant LEVEL.
+     * None: 0; ERROR: 1; WARN: 2; INFO: 3; DEBUG: 4;
+     */
+    private static final int LEVEL = 4;
+
+    /**
+     * Sends a debug message.
+     *
+     * @param msg the msg to be sent
+     */
+    public static void debug(String msg) {
+        if (LEVEL >= 4) {
+            Log.d(TAG + ":" + DEBUG, msg);
+        }
+    }
+
+    /**
+     * Sends a debug message and the exception message.
+     *
+     * @param msg the msg to be sent
+     * @param e the exception
+     */
+    public static void debug(String msg, Exception e) {
+        if (LEVEL >= 4) {
+            Log.d(TAG + ":" + DEBUG, msg + ": " + e.getMessage());
+        }
+    }
+
+    /**
+     * Sends a error message.
+     *
+     * @param msg the msg to be sent
+     */
+    public static void error(String msg) {
+        if (LEVEL >= 1) {
+            Log.e(TAG + ":" + ERROR, msg);
+        }
+    }
+
+    /**
+     * Sends a error message and the exception message.
+     *
+     * @param msg the msg to be sent
+     * @param e the exception
+     */
+    public static void error(String msg, Exception e) {
+        if (LEVEL >= 1) {
+            Log.e(TAG + ":" + ERROR, msg + ": " + e.getMessage());
+        }
+    }
+
+    /**
+     * Sends a info message.
+     *
+     * @param msg the msg to be sent
+     */
+    public static void info(String msg) {
+        if (LEVEL >= 3) {
+            Log.i(TAG + ":" + INFO, msg);
+        }
+    }
+
+    /**
+     *Sends a warning message.
+     *
+     * @param msg the msg to be sent
+     */
+    public static void warning(String msg) {
+        if (LEVEL >= 2) {
+            Log.w(TAG + ":" + WARNING, msg);
+        }
+    }
+}
