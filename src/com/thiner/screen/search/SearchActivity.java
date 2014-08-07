c10185006215b7a3c685dc58985f4a00fc9891bd
rafaelrf <rafaelrf500@gmail.com>
2014-08-07 00:05:29 -0300

    Implementando callback.

--- src/com/thiner/screen/search/SearchActivity.java
+++ src/com/thiner/screen/search/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.thiner.screen.search;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -7,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.thiner.R;
 import com.thiner.asynctask.GetJSONTask;
@@ -14,40 +18,53 @@ import com.thiner.asynctask.GetJSONTask.GetJSONInterface;
 
 public class SearchActivity extends Activity implements GetJSONInterface {
 
-	Button btGoSearch;
-	EditText searchText;
+    Button btGoSearch;
+    EditText searchText;
 
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
-		// Get Refferences of Views
+        // Get Refferences of Views
 
-		// edConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        // edConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
 
-		btGoSearch = (Button) findViewById(R.id.buttonCreateAccount);
-		btGoSearch.setOnClickListener(new View.OnClickListener() {
+        btGoSearch = (Button) findViewById(R.id.buttonCreateAccount);
+        btGoSearch.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(final View v) {
-				sendGetUserInformation();
+            @Override
+            public void onClick(final View v) {
+                sendGetUserInformation();
 
-			}
+            }
 
-		});
-	}
+        });
+    }
 
-	private void sendGetUserInformation() {
-		searchText = (EditText) findViewById(R.id.TextViewSearch);
+    private void sendGetUserInformation() {
+        searchText = (EditText) findViewById(R.id.TextViewSearch);
 
-		final String username = "username=" + searchText.getText().toString();
-		new GetJSONTask(this).execute(username);
-	}
+        final String username = "username=" + searchText.getText().toString();
+        new GetJSONTask(this).execute(username);
+    }
 
-	@Override
-	public void callbackDownloadJSON(final JSONObject json) {
+    @Override
+    public void callbackDownloadJSON(final JSONObject json) {
+        final TextView emptySearch;
+        final ListView listSearch;
 
-	}
+        try {
+            final JSONArray users = json.getJSONArray("users");
+            if (users.length() == 0) {
+                emptySearch = (TextView) findViewById(R.id.empty);
+                emptySearch.setVisibility(View.VISIBLE);
+            } else {
+                listSearch = (ListView) findViewById(R.id.list);
+            }
+        } catch (final JSONException e) {
+            e.printStackTrace();
+        }
 
+    }
 }
